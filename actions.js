af0028477c6e0bf70f2dd5c379999156c89abc30
Przemek Gałdys <galdys.przemek@gmail.com>
2018-01-14 13:42:52 +0100

    Zadanie 19.4

--- actions.js
+++ actions.js
@@ -1,45 +1,35 @@
 import uuid from uuid; //losowe generowanie identyfikatorów
 
 //akcje
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-function addComment(text) {
-    return {
-        type: ADD_COMMENT,
-        text,
-        id: uuid.v4()
-    }
-}
+export const addComment = text => ({
+    text,
+    id: uuid.v4(),
+    type: ADD_COMMENT,
+    });
 
-function removeComment(id) {
-    return {
-        type: REMOVE_COMMENT,
-        id
-    }
-}
+export const removeComment = id => ({
+    id,
+    type: REMOVE_COMMENT,
+});
 
-function editComment(id, text) {
-    return {
-        type: EDIT_COMMENT,
-        id,
-        text
-    }
-}
+export const editComment = (id, text) => ({
+    id,
+    text,
+    type: EDIT_COMMENT,
+});
 
-function thumbUpComment(id) {
-    return {
-        type: THUMB_UP_COMMENT,
-        id
-    }
-}
+export const thumbUpComment = id => ({
+    id,
+    type: THUMB_UP_COMMENT,
+});
 
-function thumbDownComment(id) {
-    return {
-        type: THUMB_DOWN_COMMENT,
-        id
-    }
-}
+export const thumbDownComment = id => ({
+    id,
+    type: THUMB_DOWN_COMMENT,
+});
